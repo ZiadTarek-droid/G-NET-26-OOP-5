7001abd08f61137468f3f897a3d5352fd16dffa0
zeyad tarek <vetogamer5@outlook.com>
2026-03-04 15:29:09 +0200

    Q1

--- Program.cs
+++ Program.cs
@@ -1,10 +1,37 @@
-﻿namespace ConsoleApp1
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q1
+            //What is an interface in C#?
+
+            //An interface in C# is a contract that defines a set of methods, properties, or events that a class must implement.
+            //It contains only method/property signatures (no implementation).
+            //------------------------------------------------------
+            //Example:
+            //interface IPrintable
+            //        {
+            //            void Print();
+            //        }
+            //        Any class that implements this interface must provide an implementation for Print().
+            //-------------------------------------------------------
+            //Why do we use interfaces instead of concrete classes directly?
+            //We use interfaces to reduce tight coupling and depend on abstractions instead of implementations.
+            //At least three benefits of using interfaces:
+            //Loose Coupling
+            //Classes depend on abstractions, not concrete implementations → easier to modify or replace components.
+            //Polymorphism
+            //Multiple classes can implement the same interface and be treated uniformly.
+            //Testability
+            //Interfaces allow mocking in unit testing.
+            //Multiple Inheritance Support
+            //C# does not support multiple class inheritance, but a class can implement multiple interfaces.
+            #endregion
         }
     }
 }
