37a3515fe68a885deac536430eda0d79f19e7f3e
zeyad tarek <vetogamer5@outlook.com>
2026-03-04 15:31:53 +0200

    Q2

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using static System.Runtime.InteropServices.JavaScript.JSType;
 using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace ConsoleApp1
 {
@@ -32,6 +33,57 @@ namespace ConsoleApp1
             //Multiple Inheritance Support
             //C# does not support multiple class inheritance, but a class can implement multiple interfaces.
             #endregion
-        }
+
+            #region Q2
+            //a) What is the problem with this design?
+
+            //Both interfaces(IEnglishSpeaker and IArabicSpeaker) contain a method called Greet().
+            
+            //Currently, the class implements one public method:
+            
+            //public void Greet()
+            //        {
+            //            Console.WriteLine("Hello / Ahlan");
+            //        }
+            
+            //        This single implementation satisfies both interfaces.
+            
+            //        Problem:
+            //We cannot provide different behavior for each interface. Both will call the same method.
+            
+            //b) How to fix it?
+            
+            //We use Explicit Interface Implementation.
+            
+            //class Translator : IEnglishSpeaker, IArabicSpeaker
+            //        {
+            //            void IEnglishSpeaker.Greet()
+            //            {
+            //                Console.WriteLine("Hello");
+            //            }
+            
+            //            void IArabicSpeaker.Greet()
+            //            {
+            //                Console.WriteLine("Ahlan");
+            //            }
+            //        }
+            
+            //        This technique is called:
+            // Explicit Interface Implementation
+            
+            //c) Can we call translator.Greet() directly?
+            
+            // No.
+            
+            //Because explicit implementations are not public methods of the class itself.
+            
+            //You must cast to the interface:
+            
+            //Translator translator = new Translator();
+            
+            //        ((IEnglishSpeaker) translator).Greet();  Hello
+            //        ((IArabicSpeaker) translator).Greet();   Ahlan
+        #endregion
+    }
     }
 }
